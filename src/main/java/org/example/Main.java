97c14d6111f43e4cc1b4cea57f5302ff6d8c1ddc
saikiran <pallasaikiranmkl@gmail.com>
2025-07-29 16:47:59 +0530

    Filter method example for user class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,21 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.example.model.User;
+
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Stream<User> usersStream = Stream.of(new User("Alice", true),
+                new User("Bob", false),
+                new User("Carline", true),
+                new User("David", false)
+        );
+
+        // filter active users
+        Stream<User> activeUsers = usersStream.filter(User::isActive);
+        activeUsers.forEach(System.out::println);
+
     }
 }
---
+++ src/main/java/org/example/model/User.java
@@ -0,0 +1,35 @@
+package org.example.model;
+
+public class User {
+    String name;
+    boolean active;
+
+    public User(String name, boolean active) {
+        this.name = name;
+        this.active = active;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", active=" + active +
+                '}';
+    }
+}
